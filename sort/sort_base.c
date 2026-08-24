97bcc0ce48aa65521a2b1609d9536ab36f05e1d8
Mayara Lima Martins <malima-m@c3r10p4.42sp.org.br>
2026-08-24 19:00:44 -0300

    atualizacaoo

--- sort/sort_base.c
+++ sort/sort_base.c
@@ -6,7 +6,7 @@
 /*   By: malima-m <malima-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/12 19:02:39 by malima-m          #+#    #+#             */
-/*   Updated: 2026/08/20 18:21:23 by malima-m         ###   ########.fr       */
+/*   Updated: 2026/08/24 18:55:13 by malima-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,56 @@ void	sort_three(t_stack **a)
 	}
 }
 
-// static int	find_min_position(t_stack *a)
-// {
+static int	find_min_position(t_stack *a)
+{
+	int	i; //contador
+	int	pos; //guardar a posição
+	int	min;
+	t_node	*actual;
 
-// }
+	if (!a || !a->top)
+		return (0);
+	i = 0;
+	pos = 0;
+	actual = a->top;
+	min = actual->value;
+	while (actual)
+	{
+		if (actual->value < min)
+		{
+			min = actual->value;
+			pos = i;
+		}
+		i++;
+		actual = actual->next;
+	}
+	return (pos);
+}
+
+static void	move_min_to_top(t_stack **a)
+{
+	int	pos;
+
+	pos = find_min_position(*a);
+	while (pos != 0)
+	{
+		if (pos <= (*a)->size / 2)
+			ra(a);
+		else
+			rra(a);
+		pos = find_min_position(*a);
+	}
+}
+
+void	sort_five(t_stack **a, t_stack **b)
+{
+	if (!a || !*a)
+		return ;
+	move_min_to_top(a);
+	pb(a, b);
+	move_min_to_top(a);
+	pb(a, b);
+	sort_three(a);
+	pa(a, b);
+	pa(a, b);
+}
