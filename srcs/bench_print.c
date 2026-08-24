e6acbdefaef863dc7e7dd78128fbf004d6831150
Aline Raissa Alves Gardenal <araissa-@c3r10p2.42sp.org.br>
2026-08-24 19:00:54 -0300

    nova função strategy_adaptive

--- srcs/bench_print.c
+++ srcs/bench_print.c
@@ -1,33 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bench_print.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: araissa- <araissa-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/24 18:07:15 by araissa-          #+#    #+#             */
+/*   Updated: 2026/08/24 18:59:20 by araissa-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 static void	print_disorder(t_context *context, int fd)
 {
-	char *str_disorder;
+	char	*str_disorder;
 
 	str_disorder = convert_disorder(context->disorder_result);
-	if(!str_disorder)
+	if (!str_disorder)
 		return ;
-	ft_putstr_fd("[bench] disorder: ",fd);
-	ft_putstr_fd(str_disorder,fd);
-	ft_putstr_fd("%",fd);
-	ft_putstr_fd("\n",fd);
+	ft_putstr_fd("[bench] disorder: ", fd);
+	ft_putstr_fd(str_disorder, fd);
+	ft_putstr_fd("%", fd);
+	ft_putstr_fd("\n", fd);
 	free(str_disorder);
 }
 
-static char	*strategy_name(t_mode mode)
+static char	*strategy_adaptive(t_context *context)
 {
-	if (mode == SIMPLE)
+	if (context->mode == ADAPTIVE)
+	{
+		if (context->strategy_used == SIMPLE)
+			return ("Adaptive / (O(n²)");
+		if (context->strategy_used == MEDIUM)
+			return ("Adaptive / O(n√n)");
+		if (context->strategy_used == COMPLEX)
+			return ("Adaptive / O(n log n)");
+	}
+	return (NULL);
+}
+
+static char	*strategy_name(t_context *context)
+{
+	if (context->mode == SIMPLE)
 		return ("Simple / (O(n²)");
-	if (mode == MEDIUM)
+	if (context->mode == MEDIUM)
 		return ("Medium /  O(n√n)");
-	if (mode == COMPLEX)
+	if (context->mode == COMPLEX)
 		return ("Complex / O(n log n)");
-	return("Adaptive");
+	return (strategy_adaptive(context));
 }
 
 static void	print_op(char *label, int value, int fd)
 {
-	char *str;
+	char	*str;
 
 	str = ft_itoa(value);
 	if (!str)
@@ -37,34 +63,35 @@ static void	print_op(char *label, int value, int fd)
 	free(str);
 }
 
-static void print_total_ops(t_op_count count, int fd)
+static void	print_total_ops(t_op_count count, int fd)
 {
-	int total_op;
-	char *total;
+	int		total_op;
+	char	*total;
 
-	total_op = count.sa + count.sb + count.ss + count.pa + count.pb  + count.ra + count.rb + count.rr + count.rra + count.rrb + count.rrr;
+	total_op = count.sa + count.sb + count.ss + count.pa + count.pb
+		+ count.ra + count.rb + count.rr + count.rra + count.rrb + count.rrr;
 	total = ft_itoa(total_op);
 	ft_putstr_fd("[bench] total_ops: ", fd);
 	ft_putstr_fd(total, fd);
-	ft_putstr_fd("\n",fd);
+	ft_putstr_fd("\n", fd);
 	free(total);
 
 }
 
-static void print_strategy(t_context *context, int fd)
+static void	print_strategy(t_context *context, int fd)
 {
-	char *str_strategy;
+	char	*str_strategy;
 
-	str_strategy = strategy_name(context->strategy_used);
-	ft_putstr_fd("[bench] strategy: ",fd);
-	ft_putstr_fd(str_strategy,fd);
+	str_strategy = strategy_name(context);
+	ft_putstr_fd("[bench] strategy: ", fd);
+	ft_putstr_fd(str_strategy, fd);
 	ft_putstr_fd("\n",fd);
 }
 
 void	print_bench(t_context *context)
 {
-	t_op_count count; 
-	
+	t_op_count	count;
+
 	count = get_op_count();
 	print_disorder(context, 2);
 	print_strategy(context, 2);
@@ -75,7 +102,7 @@ void	print_bench(t_context *context)
 	print_op(" ss: ", count.ss, 2);
 	print_op(" pa: ", count.pa, 2);
 	print_op(" pb: ", count.pb, 2);
-	ft_putstr_fd("\n",2);
+	ft_putstr_fd("\n", 2);
 	ft_putstr_fd("[bench] ", 2);
 	print_op("ra: ", count.ra, 2);
 	print_op(" rb: ", count.rb, 2);
@@ -83,6 +110,5 @@ void	print_bench(t_context *context)
 	print_op(" rra: ", count.rra, 2);
 	print_op(" rrb: ", count.rrb, 2);
 	print_op(" rrr: ", count.rrr, 2);
-	ft_putstr_fd("\n",2);
-
+	ft_putstr_fd("\n", 2);
 }
